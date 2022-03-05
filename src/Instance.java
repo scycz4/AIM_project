e81ef3d793aad827585f9c0831e4a609418f6e4c
Chengyang ZHONG <scycz4@nottingham.ac.uk>
2022-03-05 16:53:27 +0000

    Initial commit

---
+++ src/Instance.java
@@ -0,0 +1,43 @@
+public class Instance {
+    public int getProfit() {
+        return profit;
+    }
+
+    public void setProfit(int profit) {
+        this.profit = profit;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    private int profit;
+    private int weight;
+    private boolean state;
+
+    public Instance(int profit,int weight){
+        this.profit=profit;
+        this.weight=weight;
+        state=false;
+    }
+
+    public Instance(){
+
+    }
+
+    public void bitFlip(){
+        state=!state;
+    }
+}
---
+++ src/Problem.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Problem {
+    private ArrayList<Instance> instance;
+    private InputStream file;
+    private int boundary;
+    public Problem(){
+        file=getClass().getResourceAsStream("test1_4_20.txt");
+        this.initialize();
+    }
+    public void setFile(String file){
+        this.file=getClass().getResourceAsStream(file);
+        this.initialize();
+    }
+    public void initialize(){
+        try {
+            BufferedReader br=new BufferedReader(new InputStreamReader(file));
+            String s=null;
+            s=br.readLine();
+            String[] instance=s.split(" ");
+            int item=Integer.parseInt(instance[0]);
+            this.instance=new ArrayList<>(item);
+            boundary=Integer.parseInt(instance[1]);
+            while((s=br.readLine())!=null){
+                instance=s.split(" ");
+                int profit=Integer.parseInt(instance[0]);
+                int weight=Integer.parseInt(instance[1]);
+                this.instance.add(new Instance(profit,weight));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSolutionAsString(){
+        StringBuilder s=new StringBuilder();
+        Iterator<Instance> it=this.instance.iterator();
+        while(it.hasNext()){
+            Instance i=it.next();
+            String s1=i.isState()?"1":"0";
+            s.append(s1);
+        }
+        return s.toString();
+    }
+
+    public int getObjectiveFunctionValue(){
+        int value=0;
+        Iterator<Instance> it=this.instance.iterator();
+        while(it.hasNext()){
+            Instance i=it.next();
+            int judge=i.isState()?1:0;
+            value+=judge*i.getProfit();
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Problem problem=new Problem();
+    }
+}
